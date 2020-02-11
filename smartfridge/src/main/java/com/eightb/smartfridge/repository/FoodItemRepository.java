4ab3ee213494bc94dadb48940497f2ea0dc1d2b0
Victor <liangviktor@icloud.com>
2020-02-10 16:40:57 -0800

    redo

--- smartfridge/src/main/java/com/eightb/smartfridge/repository/FoodItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eightb.smartfridge.repository;
-
-import com.eightb.smartfridge.model.FoodItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FoodItemRepository extends MongoRepository<FoodItem, String> {
-    public FoodItem findByName(String name);
-    public Long deleteByName(String name);
-}
