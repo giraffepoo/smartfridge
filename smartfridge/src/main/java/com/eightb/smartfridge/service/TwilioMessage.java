739ccf4d8d909b4c6025368393cdc047f72393d0
Victor <liangvictor3@gmail.com>
2020-02-13 11:41:30 -0800

    basic working Twilio

---
+++ smartfridge/src/main/java/com/eightb/smartfridge/service/TwilioMessage.java
@@ -0,0 +1,11 @@
+package com.eightb.smartfridge.service;
+
+public class Twilio {
+    String ACCOUNT_SID = "ACd435af01060ecb1c322617feae2f8ca4";
+    String AUTH_TOKEN = "f9129f571515b4204b46f568389dc906";
+
+    public Twilio() {
+    }
+
+    
+}
